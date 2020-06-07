91c2d36505b6458642e3e448f4fd6fa5b3bdc78d
Laura <laurabeatriserafim@gmail.com>
2020-06-07 14:10:27 -0300

    refactor: Improve readability of cart context

--- src/context/cart/CartContainer.tsx
+++ src/context/cart/CartContainer.tsx
@@ -4,9 +4,11 @@ import React, {
   useMemo,
   useEffect,
 } from "react";
+import useDidMount from "@rooks/use-did-mount";
 import AsyncStorage from "@react-native-community/async-storage";
 
 import formatValue from "../../utils/formatValue";
+import findItemById from "../../utils/findItemById";
 import { CART_PRODUCTS_STORAGE_KEY } from "../../constants/asyncStorage";
 import { CartProvider } from "./CartContext";
 import { Product } from "./types";
@@ -14,7 +16,7 @@ import { Product } from "./types";
 const CartContainer: React.FC = ({ children }) => {
   const [products, setProducts] = useState<Product[]>([]);
 
-  useEffect(() => {
+  useDidMount(() => {
     async function loadProducts(): Promise<void> {
       const storagedProducts = await AsyncStorage.getItem(
         CART_PRODUCTS_STORAGE_KEY,
@@ -26,98 +28,98 @@ const CartContainer: React.FC = ({ children }) => {
     }
 
     loadProducts();
-  }, []);
+  });
 
-  const saveProducts = useCallback(async (newProducts): Promise<void> => {
-    await AsyncStorage.setItem(
-      CART_PRODUCTS_STORAGE_KEY,
-      JSON.stringify(newProducts),
-    );
-  }, []);
+  useEffect(() => {
+    async function saveProducts(): Promise<void> {
+      await AsyncStorage.setItem(
+        CART_PRODUCTS_STORAGE_KEY,
+        JSON.stringify(products),
+      );
+    }
 
-  const findProductById = useCallback(
-    (id: string) => products.find(product => product.id === id),
-    [products],
-  );
+    saveProducts();
+  }, [products]);
 
   const addToCart = useCallback(
     async product => {
-      const foundProduct = findProductById(product.id);
+      const productFound = findItemById<Product>(product.id, products);
 
-      setProducts(prevProducts => {
-        const updatedProducts = foundProduct
-          ? prevProducts.map(prevProduct => ({
-            ...prevProduct,
-            quantity: prevProduct.quantity + 1,
-          }))
-          : [
-            ...prevProducts,
-            {
-              ...product,
-              quantity: 1,
-            },
-          ];
+      if (productFound) {
+        setProducts(prevProducts => (
+          prevProducts.map(prevProduct => {
+            if (prevProduct.id === productFound.id) {
+              return {
+                ...productFound,
+                quantity: productFound.quantity + 1,
+              };
+            }
 
-        saveProducts(updatedProducts);
-        return updatedProducts;
-      });
+            return prevProduct;
+          })
+        ));
+      } else {
+        setProducts(prevProducts => (
+          [...prevProducts, { ...product, quantity: 1 }]
+        ));
+      }
     },
-    [findProductById, saveProducts],
+    [
+      products,
+    ],
   );
 
-  const increment = useCallback(
-    async id => {
-      const foundProduct = findProductById(id);
+  const increment = useCallback(id => {
+    const productFound = findItemById<Product>(id, products);
 
-      const updatedProducts = products.map(product => {
-        if (foundProduct) {
+    setProducts(prevProducts => (
+      prevProducts.map(product => {
+        if (productFound?.id === product.id) {
           return {
-            ...foundProduct,
-            quantity: foundProduct.quantity + 1,
-          };
-        }
-        return product;
-      });
-
-      saveProducts(updatedProducts);
-      setProducts(updatedProducts);
-    },
-    [findProductById, saveProducts, products],
-  );
-
-  const decrement = useCallback(
-    async id => {
-      const foundProduct = findProductById(id);
-
-      const updatedProducts = products.map(product => {
-        if (foundProduct) {
-          return {
-            ...foundProduct,
-            quantity: foundProduct.quantity - 1,
+            ...product,
+            quantity: productFound.quantity + 1,
           };
         }
 
         return product;
-      });
+      })
+    ));
+  },
+  [products]);
 
-      saveProducts(updatedProducts);
-      setProducts(updatedProducts);
-    },
-    [findProductById, saveProducts, products],
-  );
+  const decrement = useCallback(id => {
+    const productFound = findItemById<Product>(id, products);
+
+    setProducts(prevProducts => (
+      prevProducts.map(product => {
+        if (productFound?.id === product.id) {
+          return {
+            ...product,
+            quantity: productFound.quantity - 1,
+          };
+        }
+
+        return product;
+      })
+    ));
+  },
+  [products]);
 
   const removeProduct = useCallback(
     (id: string): void => {
-      const filteredProducts = products.filter(product => product.id !== id);
-      saveProducts(filteredProducts);
-      setProducts(filteredProducts);
+      setProducts(prevProducts => (
+        prevProducts.filter(product => product.id !== id)
+      ));
     },
-    [products, saveProducts],
+    [],
   );
 
   const totalProductsPrice = useMemo(() => formatValue(
     products.reduce(
-      (totalPrice, product) => totalPrice + product.price * product.quantity,
+      (totalPrice, product) => {
+        const productSubtotal = product.price * product.quantity;
+        return totalPrice + productSubtotal;
+      },
       0,
     ),
   ), [products]);
